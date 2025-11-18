59aec1e48bd4f594e42d7ee756dab647977bd65b
Keith Zhang <keith.zhang.revature@gmail.com>
2025-11-18 14:03:30 -0500

    feat: added basic db connection

--- lambda_function.py
+++ lambda_function.py
@@ -1,8 +1,17 @@
 import json
+import os
+import pg8000.native
+
 
 def lambda_handler(event, context):
-    # TODO implement
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
-    }
+    db_user = "postgres"
+    db_password = os.environ["ARXIN_DB_PASS"]
+    db_url = os.environ["ARXIN_DB_URL"]
+    conn = pg8000.native.Connection(
+        db_user,
+        password=db_password,
+        host=db_url,
+    )
+
+    conn.close()
+    return {"statusCode": 200, "body": json.dumps("Hello from Lambda!")}
