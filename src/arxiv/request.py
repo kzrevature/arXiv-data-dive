601370b4c241046a54a4eb720511438ad860d110
Keith Zhang <keith.zhang.revature@gmail.com>
2025-11-24 16:13:36 -0500

    feat: added function to fetch articles from the arXiv API

--- src/arxiv/request.py
+++ src/arxiv/request.py
@@ -1,10 +1,18 @@
 import logging
+import xml.etree.ElementTree as ET
 from datetime import datetime
+
+import requests
 
 LOG = logging.getLogger()
 
 
-def build_arxiv_query_url(start_time: datetime, end_time: datetime):
+def build_arxiv_query_url(start_time: datetime, end_time: datetime) -> str:
+    """
+    Builds a valid query url targeting the arXiv API.
+
+    API manual: https://info.arxiv.org/help/api/user-manual.html
+    """
 
     arxiv_api_base_url = "http://export.arxiv.org/api/query"
 
@@ -21,3 +29,16 @@ def build_arxiv_query_url(start_time: datetime, end_time: datetime):
         "&max_results=1000"
         "&sortBy=submittedDate&sortOrder=ascending"
     )
+
+
+def fetch_articles_from_arxiv_api(start_time: datetime, end_time: datetime) -> ET:
+    """
+    Fetches a list of article entries from the arXiv API date
+    which were last updated within the given time range.
+
+    Returns the resulting XML.
+    """
+    query_url = build_arxiv_query_url(start_time, end_time)
+    response = requests.get(query_url)
+
+    return ET.fromstring(response.text)
--- test/arxiv/test_request.py
+++ test/arxiv/test_request.py
@@ -1,9 +1,28 @@
 import logging
 from datetime import datetime
+from unittest.mock import Mock, patch
 
 import pytest
 
-from arxiv.request import build_arxiv_query_url
+from arxiv.request import build_arxiv_query_url, fetch_articles_from_arxiv_api
+
+
+@pytest.fixture
+def build_query_mock():
+    with patch("arxiv.request.build_arxiv_query_url") as mock:
+        yield mock
+
+
+@pytest.fixture
+def http_get_mock():
+    with patch("requests.get") as mock:
+        yield mock
+
+
+@pytest.fixture
+def etree_fromstring_mock():
+    with patch("xml.etree.ElementTree.fromstring") as mock:
+        yield mock
 
 
 def test_build_arxiv_query_url_builds_correct_url():
@@ -29,3 +48,35 @@ def test_build_arxiv_query_url_rejects_invalid_time_range(caplog):
 
     assert len(caplog.records) == 1
     assert caplog.records[0].levelno == logging.WARNING
+
+
+def test_fetch_articles_from_arxiv_api_builds_query_with_correct_args(
+    build_query_mock, http_get_mock, etree_fromstring_mock
+):
+    input_date_1 = datetime(2000, 1, 2)
+    input_date_2 = datetime(2000, 3, 4)
+    fetch_articles_from_arxiv_api(input_date_1, input_date_2)
+    build_query_mock.assert_called_once_with(input_date_1, input_date_2)
+
+
+def test_fetch_articles_from_arxiv_api_makes_request_with_correct_url(
+    build_query_mock, http_get_mock, etree_fromstring_mock
+):
+    sample_url = "sample-query-url.com"
+    build_query_mock.return_value = sample_url
+
+    fetch_articles_from_arxiv_api(None, None)
+
+    http_get_mock.assert_called_once_with(sample_url)
+
+
+def test_fetch_articles_from_arxiv_api_parses_http_response_to_xml_string(
+    build_query_mock, http_get_mock, etree_fromstring_mock
+):
+    sample_xml = "<xml>sample xml</xml>"
+    sample_response = Mock()
+    sample_response.text = sample_xml
+    http_get_mock.return_value = sample_response
+    fetch_articles_from_arxiv_api(None, None)
+
+    etree_fromstring_mock.assert_called_once_with(sample_xml)
