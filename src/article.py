2e55384044f8e2fd5f6ee130a5583b93fe7f0190
Keith Zhang <keith.zhang.revature@gmail.com>
2025-11-25 16:12:58 -0500

    feat: created function for inserting a record into the Article table

--- src/article.py
+++ src/article.py
@@ -1,6 +1,15 @@
+from datetime import datetime
+
+
 class Article:
     # later consider: abs_link, pdf_link, cat, subcat
-    def __init__(self, id_, title, created_at, updated_at):
+    def __init__(
+        self,
+        id_: str,
+        title: str,
+        created_at: datetime,
+        updated_at: datetime,
+    ):
         self.id = id_
         self.title = title
         self.created_at = created_at
--- src/db/queries.py
+++ src/db/queries.py
@@ -1,15 +1,37 @@
+from datetime import datetime
+from time import strftime
+
 from pg8000.native import Connection
+
+from article import Article
+
+PG_TIME_FMT = "%Y-%m-%d %H:%M:%S"
 
 
 # drops Article table
 def drop_article_table(conn: Connection):
-    QUERY_STR = "DROP TABLE IF EXISTS Article;"
-    conn.run(QUERY_STR)
+    """
+    Drops the Article table.
+
+    Fails silently if the table does not exist.
+    """
+    query_str = "DROP TABLE IF EXISTS Article;"
+    conn.run(query_str)
 
 
 # creates Article table with the columns shown below
 def create_article_table(conn: Connection):
-    QUERY_STR = (
+    """
+    Builds the Article table.
+
+    Schema:
+        id:             VARCHAR(20) PK
+        title:          VARCHAR(255)
+        created_at:     TIMESTAMP
+        updated_at:     TIMESTAMP
+    """
+
+    query_str = (
         "CREATE TABLE Article ("
         "   id             VARCHAR(20) PRIMARY KEY,"
         "   title          VARCHAR(255),"
@@ -17,4 +39,23 @@ def create_article_table(conn: Connection):
         "   updated_at     TIMESTAMP"
         ");"
     )
-    conn.run(QUERY_STR)
+    conn.run(query_str)
+
+
+def insert_article(conn: Connection, article: Article) -> list:
+    """
+    Inserts an Article object into the Article table on a given Postgres connection.
+    """
+    query_str = (
+        "INSERT INTO Article (id, title, created_at, updated_at)"
+        "VALUES (:id, :title, :created_at, :updated_at)"
+    )
+
+    param_kwargs = {
+        "id": article.id,
+        "title": article.title,
+        "created_at": article.created_at.strftime(PG_TIME_FMT),
+        "updated_at": article.updated_at.strftime(PG_TIME_FMT),
+    }
+
+    return conn.run(query_str, **param_kwargs)
